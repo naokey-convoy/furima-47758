618aceb0c1c57eab68205a0101bab6249a71d28f
naokey-convoy <convoy@katch.ne.jp>
2026-08-08 19:44:52 +0900

    Add Basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    expected_username = ENV["BASIC_AUTH_USER"]
+    expected_password = ENV["BASIC_AUTH_PASSWORD"]
+
+    authenticate_or_request_with_http_basic do |username, password|
+      expected_username.present? && expected_password.present? &&
+        ActiveSupport::SecurityUtils.secure_compare(username, expected_username) &
+          ActiveSupport::SecurityUtils.secure_compare(password, expected_password)
+    end
+  end
 end
