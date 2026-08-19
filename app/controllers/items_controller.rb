015172d8cf9bb0590bc869c56c202a45151e4541
naokey-convoy <convoy@katch.ne.jp>
2026-08-19 21:48:52 +0900

    商品詳細表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,10 @@ class ItemsController < ApplicationController
     @items = Item.includes(:user, image_attachment: :blob).order(created_at: :desc)
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   def new
     @item = Item.new
   end
